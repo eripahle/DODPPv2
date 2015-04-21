58e5ad81383217f57b24703e4d595f9bfa57fb7d
eripahle <eripahle@gmail.com>
2015-04-21 09:21:31 +0700

    upload Index utama dan Config utama

---
+++ config/config.php
@@ -0,0 +1,26 @@
+<?php
+// HTTP
+define('HTTP_SERVER', '');
+
+// HTTPS
+define('HTTPS_SERVER', '');
+
+// DIR
+define('DIR_APPLICATION', '');
+define('DIR_ENGINE', '');
+define('DIR_LANGUAGE', '');
+define('DIR_TEMPLATE', '');
+define('DIR_CONFIG', '');
+define('DIR_IMAGE', '');
+define('DIR_CACHE', '');
+define('DIR_DOWNLOAD', '');
+define('DIR_UPLOAD', '');
+define('DIR_MODIFICATION', '');
+define('DIR_LOGS', '');
+
+// DB
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
+define('DB_PREFIX', '');
